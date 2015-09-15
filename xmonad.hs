f9072908c960ee9891b8d247a1355c3950eb54d4
John Swanson <john.d.swanson@gmail.com>
2015-09-14 16:18:42 -0800

    updates

--- xmonad.hs
+++ xmonad.hs
@@ -4,17 +4,20 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run
 import XMonad.Util.EZConfig(additionalKeysP)
 
+myWorkspaces    = ["1:code", "2:web"] ++ map show [3..5] ++ ["6:cal", "7:chat", "8:gmail", "9:trello"]
+
 main = do
     h <- spawnPipe "xmobar"
     xmonad $ defaultConfig {
                  manageHook = manageDocks <+> manageHook defaultConfig
-               , borderWidth = 1
-               , normalBorderColor = "black"
-               , focusedBorderColor = "white"
+               , borderWidth = 2
+               , normalBorderColor = "#839496"
+               , focusedBorderColor = "#d33682"
+               , workspaces = myWorkspaces
                , layoutHook = avoidStruts $ layoutHook defaultConfig
                , terminal = "urxvt"
                , logHook = dynamicLogWithPP $ xmobarPP {
-                   ppCurrent = xmobarColor "black" "" . wrap "[" "]"
+                   ppCurrent = xmobarColor "red" "" . wrap "[" "]"
                  , ppOrder = \(ws:l:t:_) -> [ws,l]
                  , ppOutput = hPutStrLn h }
                } `additionalKeysP`
